62cf36dd448b95d2da341e3c5d9b67455c1e8c29
Suhas More <suhas.more@joshsoftware.com>
2020-07-01 00:41:31 +0530

    Added assests and styles

--- src/component/app/App.js
+++ src/component/app/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { PersistGate } from 'redux-persist/integration/react'
 import { Provider } from "react-redux";
-import Main from "../main"
 import {store, persistor} from "../../store/index";
+import Footer from "../footer";
+import Main from "../main"
+import "../../assets/styles/";
 
 const App = () => (
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <Main />
+      <hr />
+      <Footer />
     </PersistGate>
   </Provider>
 );
---
+++ src/component/footer/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      This is the footer
+    </div>
+  );
+}
+
+export default Footer;
