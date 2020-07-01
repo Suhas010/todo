ed1316a1bf37fe0ca89074c2b88b2ab8d4170b5b
Suhas More <suhas.more@joshsoftware.com>
2020-07-02 01:10:59 +0530

    styles change to css and added icons

--- src/component/app/App.js
+++ src/component/app/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { PersistGate } from 'redux-persist/integration/react'
 import { Provider } from "react-redux";
+import 'antd/dist/antd.css';
 import {store, persistor} from "../../store/index";
 import Footer from "../footer";
 import Main from "../main"
-import "../../assets/styles/";
+import "../../assets/styles/index.css";
 
 const App = () => (
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <Main />
-      <hr />
       <Footer />
     </PersistGate>
   </Provider>
