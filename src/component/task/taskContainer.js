a713e1698bf37b2fc3dbbbece0f66e6b3b5179e4
Suhas More <suhas.more@joshsoftware.com>
2020-07-04 16:45:03 +0530

    add and edit task feature

--- src/component/task/taskContainer.js
+++ src/component/task/taskContainer.js
@@ -1,18 +1,28 @@
 import React, { useState } from 'react';
 import { Tabs, Col, Row } from 'antd';
 import { connect } from 'react-redux';
+import moment from "moment";
 import { TASK_FILTER_CATERGORY, EDIT, ADD } from './constants';
-import { DUMMY_TASKS, ACTION_TYPE } from '../../store/constants';
+import { ACTION_TYPE } from '../../store/constants';
 import TaskCard from '../common/TaskCard';
 import { dispatchAction } from '../../store/actions';
 import Modal from 'antd/lib/modal/Modal';
 import TaskForm from './taskForm';
-import { getStoredState } from 'redux-persist';
+import { createUUID, getSelectedTask } from '../../util/helper';
 const { TabPane } = Tabs;
-
+const initialTask = {
+  name: "",
+  desc: "",
+  priority: "Medium",
+  state: "Pending",
+  dueDate: moment().add(5, "days")
+};
 const TaskContainer = ({ tasks, modal, mode, dispatchAction, loading }) => {
+  console.log(tasks,"##")
 const [state, setState] = useState({
-  activeTab: "All"
+  activeTab: "All",
+  task: initialTask,
+  error: []
 });
 
   const isLoading = async function wait(ms) {
@@ -21,15 +31,28 @@ const [state, setState] = useState({
     });
   }
 
+  const handleEditClick = taskId => {
+    const task = getSelectedTask(taskId, tasks);
+    setState({
+      ...state,
+      task,
+    });
+    dispatchAction(ACTION_TYPE.TOGGLE_MODAL, { modal: true, mode: true })
+  }
+  
+  const markDone = id => {
+    console.log(id)
+  }
+
   const getTasksCards = () => {
-    let filteredTask = DUMMY_TASKS;
+    let filteredTask = tasks;
     const { activeTab } = state;
     if(activeTab !== "All") {
       filteredTask = filteredTask.filter(({state}) => state == activeTab);
       console.log(filteredTask.length,"!!")
     }
-    return filteredTask.map(({ title, createdAt, desc, dueDate, id, state }) => (
-      <Col sm={24} md={12} lg={6} span={1} key={id}>
+    return filteredTask.map(({ title, createdAt, desc, dueDate, id, state }, index) => (
+      <Col sm={24} md={12} lg={7} span={1} key={id}>
         <TaskCard
           id={id}
           desc={desc}
@@ -38,10 +61,13 @@ const [state, setState] = useState({
           dueDate={dueDate}
           createdAt={createdAt}
           loading={false}
+          handleEditClick={handleEditClick}
+          markDone={markDone}
         />
       </Col>
     ))
   };
+  
   const getTabsPane = () => {
     return TASK_FILTER_CATERGORY.map((type) => (
       <TabPane tab={type} key={type}>
@@ -50,15 +76,83 @@ const [state, setState] = useState({
         </Row>
       </TabPane>
     ))
-  }
+  };
+
+  const handleChange = (key, value) => {
+    setState({
+      ...state,
+      task: {
+        ...state.task,
+        [key]: value
+      }
+    });
+  };
+
+  const handleSave = () => {
+    const { task : { title, desc, priority, dueDate, id }} = state;
+    let error = [];
+    
+    if(!title || title.length < 10 || title.length > 140) {
+      error.push("Error in title, Must contain more than 10 and less than 140 chars");
+    }
+    if(!desc ||  desc.length < 50 || desc.length > 500) {
+      error.push("Description must contain more than 50 and less 500 chars");
+    }
+    if(error.length) {
+      setState({
+        ...state,
+        error
+      });
+      return;
+    }
+    if(id) {
+      dispatchAction(ACTION_TYPE.UPDATE_TASK, state.task);
+    } else {
+      let payload = {
+        ...state.task,
+        state: "Pending",
+        createdAt: moment(),
+        id: createUUID()
+      }
+      dispatchAction(ACTION_TYPE.ADD_TASK, payload);
+    }
+    setState({
+      ...state,
+      error
+    });
+    console.log(state.task)
+  };
+
+  const getErrors = () => {
+    const { error } = state;
+    return error.map(error => (
+      <div style={{color: "red"}}>
+        {error}
+      </div>
+    ));
+  };
+
+  const handleModalClose = () => {
+    dispatchAction(ACTION_TYPE.TOGGLE_MODAL, {modal: false, mode: true});
+    setState({
+      ...state,
+      task: initialTask,
+      error: []
+    });
+  };
+
   const getTaskModal = () => (
     <Modal
       title={mode ? EDIT : ADD}
       visible={modal}
-      onOk={() => {}}
-      onCancel={()=>dispatchAction(ACTION_TYPE.TOGGLE_MODAL, {modal: false, mode: true})}
+      onOk={handleSave}
+      onCancel={handleModalClose}
     >
-      <TaskForm />
+      {getErrors()}
+      <TaskForm 
+       task={state.task}
+       handleChange={handleChange}
+      />
     </Modal>
   );
   const { activeTab } = state;
--- src/component/task/taskForm.js
+++ src/component/task/taskForm.js
@@ -1,44 +1,80 @@
 import React from 'react';
+import moment from "moment";
 import { Input, Col, DatePicker, Row, Select } from "antd";
 import { TASK_PRIORITIES } from "./constants";
 
-const TaskForm = () => {
-  // const [state, setState] = useState({
-  //   title:"",
-  //   desc:"",
-  // });
-  const getOptions = () => {
-    return TASK_PRIORITIES.map(priority => (
+const TaskForm = ({
+ task: {
+  title,
+  desc,
+  priority,
+  dueDate,
+  state
+ },
+ handleChange
+}) => {
+  const getOptions = (options) => {
+    return options.map(priority => (
       <Select.Option value={priority} key={priority}>
         {priority}
       </Select.Option>
     ));
   }
+
   return (
     <div className="task-form">
       <Row>
         <Col ls={24} sm={24} md={24}>
           <label>Title</label>
-          <Input name="title" placeholder="task title" onChange={()=>{}} style={ {width: "100%"}}/>
+          <Input
+            name="title"
+            placeholder="task title"
+            value={title}
+            onChange={({target: {value}})=>handleChange("title", value)}
+            style={ {width: "100%"}}
+          />
         </Col>
       </Row>
       <Row>
         <Col ls={24} sm={24} md={24}>
           <label>Description</label>
-          <Input.TextArea name="desc" placeholder="describe task" style={{width: "100%"}}/>
+          <Input.TextArea
+            name="desc"
+            placeholder="describe task"
+            value={desc}
+            onChange={({target: {value}})=>handleChange("desc", value)}
+            style={{width: "100%"}}/>
+        </Col>
+      </Row>
+      <Row>
+        <Col sm={24} md={24} lg={24}>
+          <label>Priority</label>
+          <Select
+            value={state}
+            style={{ width: "100%" }}
+            onChange={(e)=>handleChange("state", e)}
+          >
+            {getOptions(["Pending", "Compleated"])}
+          </Select>
         </Col>
       </Row>
       <Row>
         <Col lg={12} sm={24} md={12}>
           <label>Task Priority</label>
-          <Select defaultValue="lucy" style={{ width: "100%" }} onChange={()=>{}}>
-            {getOptions()}
+          <Select
+            value={priority}
+            style={{ width: "100%" }}
+            onChange={(e)=>handleChange("priority", e)}
+          >
+            {getOptions(TASK_PRIORITIES)}
           </Select>
         </Col>
         <Col>
         <label>Due Date</label>
           <DatePicker
             style={{ width: "100%"}}
+            value={moment(dueDate)}
+            onChange={(e) => handleChange("dueDate", e)}
           />
         </Col>
       </Row>
